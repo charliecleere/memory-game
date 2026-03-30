d4ace728b3dbe57d8870b0c86604183f6f4cb93f
ccleere12-cmd <ccleere12@gmail.com>
2026-03-29 23:27:19 -0400

    Clarify TODO: instruction display option
    Update TODO in memory-game.py to note offering users the option to view or skip instructions.

--- memory-game.py
+++ memory-game.py
@@ -1,5 +1,5 @@
 # TODO: 
-# Fix the instructions and the display of it
+# Fix the instructions and the display of it. Also maybe give the users the option to view the instructions or not view it
 # Increase modularity (e.g., maybe do a function for displaying the instructions)
 # Maybe remove the "e.g." because some people may not know what it means. If you keep it, figure out to do with the comma after "e.g."
 # Fix the comments throughout the program and probably add more
